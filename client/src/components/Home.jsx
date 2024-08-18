aa13a0abb2836d8b72f60e6d40281fcf0278064a
Krish2707 <thokkukrishnaprasad2707@gmail.com>
2024-08-18 12:14:09 +0530

    Your commit message here

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,58 +1,53 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import convo from '../images/convo.png'
+import React, { useEffect, useState } from 'react';
+import convo from '../images/convo.png';
+import '../styles/home.css'; // Import the CSS file
 
-const Home=()=>{
-    const [userName,setUserName] =useState();
-    const [show,setShow]=useState(false);
+const Home = () => {
+    const [userName, setUserName] = useState(null);
 
-    const userHome=async()=>{
-       try{
-         const res = await fetch("/getdata",{
-            method: "GET",
-            headers:{
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            credentials:"include"
-         });
-         const data=await res.json();
-         setUserName(data.name);
-         setShow(true);
-         if(!res.status===200){
-             const error=new Error(res.err);
-             throw error;
-         }
-       }catch(err){
-           console.log(err);
-       }
+    const userHome = async () => {
+        try {
+            const res = await fetch("/getdata", {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+
+            if (res.status !== 200) {
+                const error = new Error('Failed to fetch');
+                throw error;
+            }
+
+            const data = await res.json();
+            setUserName(data.name);
+        } catch (err) {
+            console.log(err);
+        }
     }
-    useEffect(()=>{
-       userHome();
-    },[])
-   
-    if(userName){
-        return(
-            <div className="row">
-            <div className="col-md-5">
-            <img src={convo} alt="not found"/>
+
+    useEffect(() => {
+        userHome();
+    }, [])
+
+    return (
+        <div className="home-container">
+            <div className="home-content">
+                <div className="image-container">
+                    <img src={convo} alt="Conversation" className="img-fluid" />
+                </div>
+                <div className="text-container">
+                    {userName ? (
+                        <p className='welcome-text'>Hello {userName}! Welcome Back!!</p>
+                    ) : (
+                        <p className='welcome-text'>Please login to file a grievance</p>
+                    )}
+                </div>
             </div>
-            <p className='maintext'>Hello {userName}! Welcome Back!! </p>
-            </div> 
-        );
-    }
-   return(
-       <>
-       <br />
-       <div className="row">
-       <div className="col-md-5">
-       <img src={convo} alt="not found"/>
-       </div>
-       <p className='maintext' >Please login to file a grievance</p>
-       </div>
-       </>
-   )
+        </div>
+    );
 };
-
 
 export default Home;
